a4f4b059e9371bacb432bf201908606bc05a7d47
Debdut Karmakar <debdut.karmakar@gmail.com>
2018-07-28 17:55:20 +0530

    fc

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+
+  #app
+    router-view
+
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+
+</script>
+
+<style lang="sass">
+
+=smoothing($property)
+  -webkit-font-smoothing: $property
+  -moz-osx-font-smoothing: $property
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  +smoothing(antialiased)
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+</style>
---
+++ src/components/Pug.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .container
+    h1 Pug
+    h2 The Best Templating Engine
+
+    - const fruits = ['apple', 'mango', 'grape']
+    select
+      each fruit in fruits
+        option= fruit
+
+    - const numFriends = 10
+    case numFriends
+      when 0
+        p Pug has no friends
+      when 1
+        p Pug has a friend
+      default
+        p Pug has #{numFriends} friends
+</template>
+
+<style lang="sass" scoped>
+</style>
